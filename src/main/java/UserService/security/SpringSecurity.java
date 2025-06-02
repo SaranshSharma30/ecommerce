cf9a011cb6db08e138d2c410901f3f2a05fdd92a
SaranshSharma30 <saransh.sharma249@gmail.com>
2025-06-03 02:42:52 +0530

    Refactoring.

--- src/main/java/UserService/security/SpringSecurity.java
+++ src/main/java/UserService/security/SpringSecurity.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class springSecurity {
+public class SpringSecurity {
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
